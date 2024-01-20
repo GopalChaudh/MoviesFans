5f2509a1192ce7ff9cbd780f395a629336caef38
Gopal Chaudhary <112993505+GopalChaudh@users.noreply.github.com>
2024-01-20 13:02:01 +0530

    success fully added search button

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createRoot } from 'react-dom'; // Import from 'react-dom' instead of 'react-dom/client'
+import React,{Suspense,lazy} from 'react'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import Footer from './Components/Footer/Footer';
 import Loading from './Components/Loading/Loading';
-import Home from "./Components/Home/Home";
-import './index.css';
+import Home from "./Components/Home/Home"
+import './index.css'
+const Favorate = lazy(() =>import('./Components/Favorate/Favorate'))
+const NavBar = lazy(() => import('./Components/NavBar/NavBar'))
+const Search = lazy(()=>import('./Components/search/Search'))
 
-const Favorate = lazy(() => import('./Components/Favorate/Favorate'));
-const NavBar = lazy(() => import('./Components/NavBar/NavBar'));
-const Search = lazy(() => import('./Components/search/Search'));
+
+
 
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <Suspense fallback={<Loading />}>
-      <NavBar />
-      <Routes>
-        {/* Add 'exact' prop to the home route */}
-        <Route path='/' element={<Home />}  />
-        <Route path='/MoviesFans' element={<Home />}  />
-        <Route path="/favorate" element={<Favorate />} />
-        <Route path='/search' element={<Search />} />
-      </Routes>
-    </Suspense>
-    <Footer />
-  </Router>
-);
+        <Router>
+            
+            <NavBar />
+                <Suspense fallback={<Loading />} >
+            <Routes>
+
+                <Route path='/' element={<Home />} />
+                <Route path="/favorate" element={<Favorate />}/>
+                <Route path='/search' element={<Search />} />
+
+            </Routes>
+                </Suspense>
+            <Footer />
+        </Router>
+)
